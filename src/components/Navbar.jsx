f649e5dbaf398c782403c18a5fa99f133370fdca
52330918-cloud <52330918@students.liu.edu.lb>
2025-11-23 20:57:30 +0200

    preparing nav with nav tag that is container for everything and the first div thst will be on the left of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 
 function Navbar({onLogout}) {
@@ -9,7 +10,20 @@ function Navbar({onLogout}) {
     navigate('/');
   };
   return (
- <></>
+ <nav className="bg-gradient-to-r from-blue-500 via-red-300 to-purple-400 shadow-lg px-8 py-3 flex items-center justify-between w-full sticky top-0 z-50">
+ <div className="flex items-center gap-2">
+        <span className="text-2xl font-bold text-white">🎓</span>
+        <span className="text-xl font-semibold text-white  drop-shadow">Student Portal</span>
+      </div>
+
+
+
+
+
+
+
+
+ </nav>
   )
 }
 
